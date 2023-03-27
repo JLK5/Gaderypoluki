c97f6f5206badb8ac84fcf5b79b0fae14d4cc19a
JLK5 <jlkborkowski@gmail.com>
2023-03-27 12:28:50 +0200

    V2

--- Gaderypoluki/Program.cs
+++ Gaderypoluki/Program.cs
@@ -1,7 +1,27 @@
 ﻿string text = "ALA MA KOTA";
-text = text.Replace('G', 'A');
+text = text.Replace('G', '@');
 text = text.Replace('A', 'G');
+text = text.Replace('@', 'A');
 
+text = text.Replace('D', '@');
+text = text.Replace('E', 'D');
+text = text.Replace('@', 'E');
+
+text = text.Replace('R', '@');
+text = text.Replace('Y', 'R');
+text = text.Replace('@', 'Y');
+
+text = text.Replace('P', '@');
+text = text.Replace('O', 'P');
+text = text.Replace('@', 'O');
+
+text = text.Replace('L', '@');
+text = text.Replace('U', 'L');
+text = text.Replace('@', 'U');
+
+text = text.Replace('K', '@');
+text = text.Replace('I', 'K');
+text = text.Replace('@', 'I');
 
 
 Console.WriteLine(text);
